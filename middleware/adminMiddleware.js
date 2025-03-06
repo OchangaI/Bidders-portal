81b0f1cf7735f9eb10e9f93c0ba0bb1e2078c3a2
Grd-32 <pfdaniel77@gmail.com>
2025-03-06 16:31:57 +0300

    adjusting index.js file

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = 'https://cbdbitjcgjlvcezcnojh.supabase.co';
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
